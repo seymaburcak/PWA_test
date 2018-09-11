e1c620398b3520b700752c79b93c55f62e9158a2
Seyma Cetinkaya <seymaburcak@mynet.com>
2018-09-11 14:07:52 +0300

    Headlines+Articles

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var request=new XMLHttpRequest();
+request.open("GET","C:/Users/Şeyma/Downloads/feed.json",false);
+request.send(null);
+var JSON_object=JSON.parse(request.responseText);
+var i=0;
+for(i;i<6;i++)
+{
+
+}
